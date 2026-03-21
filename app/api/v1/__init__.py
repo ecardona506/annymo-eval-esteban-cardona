5f99d471883ed06ce43a09da6d7c8a14e11b87d0
ecardona506 <ecardona506@javerianacali.edu.co>
2026-03-21 12:21:58 -0500

    update users path according to version

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,4 +3,4 @@ from app.api.v1.routes.user import ns as user
 
 def register_v1(api: Api):
     """Register API namespaces for version 1."""
-    api.add_namespace(user, path='/user')
+    api.add_namespace(user, path='/api/v1/user')
