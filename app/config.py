8cdb4e25652efa99e70ecc6d94a61ea3779034d7
ecardona506 <ecardona506@javerianacali.edu.co>
2026-03-21 17:57:56 -0500

    fix env variable misconfiguration

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     """Base configuration."""
     SECRET_KEY = os.getenv("SECRET_KEY", "my_secret_key")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///app.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True  # Ensure exceptions propagate to error handlers
 
