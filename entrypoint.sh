51e2dc58131f820d721d9f46d333c2f8956cc5ab
ecardona506 <ecardona506@javerianacali.edu.co>
2026-03-21 17:57:01 -0500

    updat enetrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "Running migrations..."
-exec uv run flask db upgrade
+echo "Running migrations"
+uv run flask db upgrade
 
-echo "Starting server..."
-exec uv run flask run --host=0.0.0.0
+echo  "Starting server"
+uv run flask run --host=0.0.0.0 --port=5000
