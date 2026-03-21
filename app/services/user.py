3619da8a73992f4a5de0d1bac2114f38082cfdcd
ecardona506 <ecardona506@javerianacali.edu.co>
2026-03-20 20:22:58 -0500

    add user service

---
+++ app/services/user.py
@@ -0,0 +1,18 @@
+from app.models.user import User
+from app.extensions import db
+from flask_bcrypt import generate_password_hash
+
+class UserService:
+    @staticmethod
+    def create_user(user_data: dict) -> User:
+        """Create a new user and save to the database"""
+        user = User(**user_data)
+        user.password = UserService.hash_password(user)
+        db.session.add(user)
+        db.session.commit()
+        return user
+    
+    @staticmethod
+    def hash_password(user: User) -> str:
+        """Hash the user's password"""
+        return generate_password_hash(user.password).decode('utf-8')
