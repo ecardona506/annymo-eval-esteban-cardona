ad01067f2d27d644d1f89762eb5b5e23c910015a
ecardona506 <ecardona506@javerianacali.edu.co>
2026-03-22 10:39:52 -0500

    add user schemas for all user endpoint

--- app/api/v1/schemas/user.py
+++ app/api/v1/schemas/user.py
@@ -20,4 +20,17 @@ def register_user_schemas(ns: Namespace):
         'password': fields.String(required=True, description='The user password'),
     })
 
-    return user_response_schema, create_user_schema
+    # Schema for paginated user list
+    pagination_schema = ns.model('Pagination', {
+        'page': fields.Integer(description='Current page number'),
+        'per_page': fields.Integer(description='Number of items per page'),
+        'total': fields.Integer(description='Total number of items'),
+        'pages': fields.Integer(description='Total number of pages'),
+    })
+
+    users_list_schema = ns.model('UsersList', {
+        'users': fields.List(fields.Nested(user_response_schema), description='List of users'),
+        'pagination': fields.Nested(pagination_schema, description='Pagination information'),
+    })
+
+    return user_response_schema, create_user_schema, users_list_schema
